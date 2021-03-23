48f352a731f72c3ffde6f0d86e65616415fb8f70
Joel Wanjala <joel.n.wanjala@gmail.com>
2021-03-23 15:11:58 -0700

    My personal portfolio

---
+++ resources/js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+    $('.toggle').hide();
+
+    $('.btn-toggle').click(function () {
+
+        $('.img-responsive').toggle();
+
+        $('.toggle').toggle();
+
+    });
+
+});
